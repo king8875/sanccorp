6abff2cab7484deb80738c55621ae024b1e1d497
SSIM <ssim@SSIMui-Macmini.local>
2025-10-20 18:53:31 +0900

    good

--- test.js
+++ test.js
@@ -1,24 +1,20 @@
-const tabItems = document.querySelectorAll('.tab-item');
-const tabPanels = document.querySelectorAll('.tab-panel');
+const tabItems = document.querySelectorAll(".tab-item");
+const tabPanels = document.querySelectorAll(".tab-panel");
 
-tabItems.forEach(tab => {
-    tab.addEventListener('click', function(){
-        tabItems.forEach(item => item.classList.remove('active'));
-        tabPanels.forEach(panel => panel.classList.remove('active'));
+tabItems.forEach((tab) => {
+  tab.addEventListener("click", function () {
+    tabItems.forEach((item) => item.classList.remove("active"));
+    tabPanels.forEach((panel) => panel.classList.remove("active"));
 
-        this.classList.add('active');
+    this.classList.add("active");
 
-        const target = this.dataset.tab;
+    const target = this.dataset.tab;
 
-        const targetPanel = document.getElementById(target);
-        if(targetPanel) {
-            targetPanel.classList.add('active');
-        }
-    })
-})
-
-
-
-
+    const targetPanel = document.getElementById(target);
+    if (targetPanel) {
+      targetPanel.classList.add("active");
+    }
+  });
+});
 
 
